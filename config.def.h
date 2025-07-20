1f27f68b9383688244c361186570889833aff98f
Ibrahim Johar <ibbe.johar@gmail.com>
2025-07-20 16:34:24 +0200

    test

--- config.def.h
+++ config.def.h
@@ -23,7 +23,7 @@ static int log_level = WLR_ERROR;
 
 /* Autostart */
 static const char *const autostart[] = {
-        "bash", "-c", "swaybg", "-i", "home/fool/Pictures/wallpaper/daniel-leone.jpg", NULL,
+        "bash", "-c", "swaybg -i home/fool/Pictures/wallpaper/daniel-leone.jpg", NULL,
         NULL /* terminate */
 };
 

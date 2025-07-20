8e9f618ac741ceee851b8b51faae2dd3b41adf56
Ibrahim Johar <ibbe.johar@gmail.com>
2025-07-20 16:45:50 +0200

    comma missing

--- config.def.h
+++ config.def.h
@@ -128,7 +128,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "alacritty", NULL };
-static const char *menucmd[] = { "bemenu-run", "-c", "-l", "5", "-W", "0.2" NULL };
+static const char *menucmd[] = { "bemenu-run", "-c", "-l", "5", "-W", "0.2", NULL };
 
 #include "keys.h"
 static const Key keys[] = {
